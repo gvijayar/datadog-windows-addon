58bf071fd9c647ffaed8321b1e9472d2d382a378
Gvijayar <gvijayar@users.noreply.github.com>
2018-04-15 15:59:42 -0500

    Update pre-start.ps1

--- jobs/dd-win-agent/templates/pre-start.ps1
+++ jobs/dd-win-agent/templates/pre-start.ps1
@@ -1,1 +1,67 @@
-powershell Unblock-File .\Datadog-installer.ps1 -APIKey b28823b1e71c44ddba8ebf76c1f1f919
+<#
+    .SYNOPSIS
+    This PowerShell script is designed to install the latest Datadog agent seamlessly on windows
+
+    .PARAMETER APIKey
+    The API Key for your datadog installation. Contact support if you don't have one!
+
+    .PARAMETER Location
+    Where the MSI should be dropped. Defaults to C:\Windows\temp
+
+    .PARAMETER Hostname
+    Name of the host. Defaults to the Windows hostname
+
+    .PARAMETER Tags
+    Tags to assign.
+
+    .EXAMPLE
+    .\Datadog-Installer.ps1 -APIKey INSERTKEYHERE -Tags MyTag1,MyTag2
+#>
+[CmdletBinding()]
+Param(
+
+    [string]   $APIKey = "b28823b1e71c44ddba8ebf76c1f1f919",
+
+    [ValidateScript ( { Test-Path $_ } ) ]
+    [string]   $Location = "C:\Windows\Temp",
+    [string]   $Hostname = $env:COMPUTERNAME,
+    [string[]] $Tags = "CFSummit,Bosh,Windows"
+)
+begin{
+    $MSI = "$location\ddog.msi"
+    If ( Test-path $MSI ) {
+        Remove-Item $MSI -Force
+    }
+}
+process{
+
+Write-Output "Getting versions JSON"
+$InstallerJsonUrl ="https://s3.amazonaws.com/ddagent-windows-stable/installers.json"
+$Req = Invoke-WebRequest -Uri $InstallerJsonUrl -Method Get -UseBasicParsing
+
+Write-Output "Calculating latest version"
+$VersionsObject = $Req.Content | ConvertFrom-Json
+$VersionStrings = $VersionsObject| Get-Member -MemberType NoteProperty | Select Name
+
+$Versions = @()
+ForEach ($Version in $VersionStrings){
+    $Versions += [version] $Version.Name
+}
+
+$LatestVersion = $Versions | Sort-Object -Descending | Select -First 1
+Write-Output "Collecting Datadog Version $($LatestVersion.ToString())"
+$TargetMSI = $VersionsObject.$($LatestVersion.toString()).amd64
+
+Invoke-WebRequest -Uri $TargetMSI -OutFile $MSI -Method Get -UseBasicParsing
+
+If ( $Tags ){
+    $Expression = "msiexec /qn /i `"$MSI`" APIKEY=`"$APIKEY`" HOSTNAME=`"$HOSTNAME`" TAGS=`"$($TAGS -join ",")`""
+}
+Else{
+    $Expression = "msiexec /qn /i `"$MSI`" APIKEY=`"$APIKEY`" HOSTNAME=`"$HOSTNAME`""
+}
+Write-Output "Commencing Installation"
+Write-Verbose "Installation Command: $Expression"
+Invoke-Expression $Expression
+Write-Output "Installation Completed"
+}
